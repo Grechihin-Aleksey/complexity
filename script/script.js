0f4f497dec43d8b4fc867c62d1e1f2ff4034853c
Aleksey Grechihin <9108921170@dz-led.ru>
2020-08-07 22:50:14 +0300

    Исправил дополнительное задание

--- script/script.js
+++ script/script.js
@@ -1,10 +1,12 @@
-let num = [2, 6, 6, 2, 1, 9];
+let num = 266219;
+let num_1 = String(num);
+let num_2 = num_1.split("");
 
-let result = num.reduce(function (a, b) {
-    return a * b;
-})
+let result = num_2.reduce(function (a, b) {
+  return a * b;
+});
 
 console.log(result);
 let name = result ** 3;
-let name_1 = String(name).slice(0, 2)
+let name_1 = String(name).slice(0, 2);
 console.log(name_1);

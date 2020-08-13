da125895100210e9ec6fc6135bc7944e3e4b05ae
Aleksey Grechihin <9108921170@dz-led.ru>
2020-08-13 11:52:27 +0300

    Дополнительное №050

--- script/script.js
+++ script/script.js
@@ -1,13 +1,24 @@
-let lang = "                                            Привет";
-let num = function (a) {
-  if (typeof a === "string" && a.length < 30) {
-    console.log(lang.trim());
+let arr = ["32", "27", "885", "104", "47", "78", "477"];
+
+arr.forEach(function (item) {
+  if (item.startsWith("2") || item.startsWith("4")) {
+    console.log(item);
   }
-  if (typeof a === "string" && a.length > 30) {
-    console.log(lang.trim().substr(0, 10) + "...");
+});
+
+let n = 1;
+
+while (n <= 100) {
+  if (n !== 1) {
+    let result = 2;
+    let i = 2;
+    while (n % i !== 0) {
+      i += 1;
+      result += 1;
+    }
+    if (result == n) {
+      console.log(n + " делитель этого числа 1 и " + n);
+    }
   }
-  if (typeof a !== "string") {
-    console.log("не строка");
-  }
-};
-num(lang);
+  n += 1;
+}

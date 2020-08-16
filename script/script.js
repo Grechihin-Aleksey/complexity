8814472ce03bdb287a41a366eb3a709626b6d6fe
Aleksey Grechihin <9108921170@dz-led.ru>
2020-08-16 10:34:53 +0300

    Исправил

--- script/script.js
+++ script/script.js
@@ -1,23 +1,26 @@
 "use strict";
 
 let week = [
+  "Воскресенье",
   "Понедельник",
   "Вторник",
   "Среда",
   "Четверг",
   "Пятница",
   "Суббота",
-  "Воскресенье",
 ];
+
+let d = new Date();
+const dayIndex = d.getDay();
+
+const dayName = week[dayIndex];
 
 for (let i = 0, len = week.length; i < len; i++) {
   let html = week[i];
-  if (i === 1) html = html.bold();
-  else if (i > 4) html = html.italics();
+  if (i === dayIndex) html = html.bold();
+  else if (i === 0 || i === 6) html = html.italics();
 
   const div = document.createElement("div");
   div.innerHTML = html;
   document.body.appendChild(div);
 }
-let date = new Date(2020, 7, 18);
-console.log(date);
